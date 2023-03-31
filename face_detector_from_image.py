951d642c07a9bf2efe7b8ce90491baf4b1dfa4d3
sabrinachowdhuryoshin <sabrina.chowdhury29@gmail.com>
2023-03-31 22:53:38 +0100

    app init 🎉

--- face_detector_from_image.py
+++ face_detector_from_image.py
@@ -1,27 +1,50 @@
-import cv2
+'''' standard python library '''
+
+import os, cv2
+from pathlib import Path
 from random import randrange
 
-# load the cascade
+# %%
+
+'''' define directory paths '''
+
+tool_path = os.path.dirname(os.path.abspath(__file__))
+# print(tool_path) # debug
+
+img_path = Path(tool_path + "\\image")
+# print(img_path ) # debug
+
+algprtithm_path = Path(tool_path + "\\algorithm")
+# print(algorithm_path ) # debug
+
+# %%
+# load the cascade algorithm
 # haarcascade algorithm only takes the gray scale images
-trained_face_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+trained_face_data = cv2.CascadeClassifier(str(algprtithm_path) + "\\haarcascade_frontalface_default.xml")
+# print(trained_face_data) # debug
 
 # choose an image to detect faces in
-# img = cv2.imread("Robert-Downey-Jr.png")
-img = cv2.imread("Multiple_Faces_01.jpg")
+# img = cv2.imread(str(img_path) + "\\Robert_Downey_Jr.png")
+# img = cv2.imread(str(img_path) + "\\Multiple_Faces_01.jpg")
+img = cv2.imread(str(img_path) + "\\Multiple_Faces_02.jpg")
+# print(img) # debug
 
 # show the image 
 cv2.imshow("Clever Programmer Face Detector", img)
-cv2.waitKey(1000)
+cv2.waitKey(1000) # debug
 
 # must convert to grayscale
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# print(grayscaled_img) # debug
 
 # show the grayscaled image
 cv2.imshow("Clever Programmer Face Detector", grayscaled_img)
-cv2.waitKey(1000)
+cv2.waitKey(1000) # debug
 
+# %%
 # detect faces
 face_coordinates  = trained_face_data.detectMultiScale(grayscaled_img)
+# print(face_coordinates) # debug
 
 # draw rectangles around the faces
 # (x,y) - upper left point
@@ -29,13 +52,11 @@ face_coordinates  = trained_face_data.detectMultiScale(grayscaled_img)
 # 2 - thickness of the rectangle
 # get the face coordinates dynamically
 for (x,y,w,h) in face_coordinates:
-
     cv2.rectangle(img, (x,y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2)
-
-# print(face_coordinates)
 
 # display the image with the faces
 cv2.imshow("Clever Programmer Face Detector", img)
 cv2.waitKey(1000)
 
-print("Code Completed")
+# %%
+print("\nCode Completed!\n")
